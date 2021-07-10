e219950d372749fdb9970c3d0256d6f686ae8d3c
yash <rshailu.yash@gmail.com>
2021-07-09 20:15:09 -0400

    Initialized User Tracker Handle

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use nite2_sys;
+use nite2_sys::NiteUserTracker;
+use nite2_sys::NiteUserTrackerHandle;
+
 fn main() {
-    println!("Hello, world!");
+    let mut userTracker:NiteUserTracker = unsafe {std::mem::zeroed()};
+    let mut userTrackerHandle:NiteUserTrackerHandle = &mut userTracker;
+    let mut niteRc:nite2_sys::NiteStatus;
+
+    unsafe {
+        nite2_sys::niteInitialize();
+        niteRc = nite2_sys::niteInitializeUserTracker(&mut userTrackerHandle);
+    }
+
+    if (niteRc != nite2_sys::NITE_STATUS_OK){
+        println!("Couldn't create user tracker");
+        std::process::exit(3);
+    }
 }
