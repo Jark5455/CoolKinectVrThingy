0ffcd9c31d74c49476b41588507708d6df06ef2b
yash <rshailu.yash@gmail.com>
2021-07-19 16:36:49 -0400

    IT WORKS LES GO

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
     println!("cargo:rustc-link-lib=dylib=NiTE2");
     println!("cargo:rustc-link-lib=dylib=OpenNI2");
-    println!("cargo:rustc-link-lib=dylib=FreenectDriver");
+    println!("cargo:rustc-link-lib=dylib=nite4vr");
     println!("cargo:rustc-link-search=native=/home/yashr/Documents/CoolKinectVrThingy/libs/NiTE-Linux-x64-2.2/Samples/Bin/");
-    println!("cargo:rustc-link-search=native=/home/yashr/Documents/CoolKinectVrThingy/libs/NiTE-Linux-x64-2.2/Samples/Bin/OpenNI2/Drivers/");
+    println!("cargo:rustc-link-search=native=/home/yashr/Documents/CoolKinectVrThingy/libs/libnite4vr/");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,4 @@
-mod NiTE;
-
-use NiTE::niteInitialize;
-use NiTE::nite_UserTracker;
-use NiTE::nite_UserTrackerFrameRef;
-use NiTE::NiteStatus;
-use NiTE::NiteStatus_NITE_STATUS_OK;
-use NiTE::NiteUserTracker;
+mod nite4vr;
 
 use termios::tcgetattr;
 use termios::tcsetattr;
@@ -26,77 +19,24 @@ use libc::STDIN_FILENO;
 use libc_stdhandle::stdin;
 
 use std::mem::MaybeUninit;
+use std::thread;
+use std::time;
 
 fn main() {
-    let mut niteUserTracker: MaybeUninit<NiteUserTracker> =
-        MaybeUninit::<NiteUserTracker>::uninit();
-
-    let mut userTracker: nite_UserTracker = nite_UserTracker {
-        m_userTrackerHandle: &mut niteUserTracker,
-    };
-    let mut niteRc: NiteStatus;
-
-    unsafe {
-        niteInitialize();
-        niteRc = userTracker.create(None);
-    }
-
-    if niteRc != NiteStatus_NITE_STATUS_OK {
-        println!("Couldn't create user tracker");
-        dbg!(niteRc);
-        std::process::exit(3);
-    }
-
-    println!("Start moving around to get detected...");
-    println!("(PSI pose may be required for skeleton calibration, depending on the configuration)");
-
-    let mut userTrackerFrame: MaybeUninit<nite_UserTrackerFrameRef> =
-        MaybeUninit::<nite_UserTrackerFrameRef>::uninit();
-    let mut userTrackerFrameRef = userTrackerFrame.as_mut_ptr();
-
+    thread::spawn(|| {unsafe { nite4vr::startTracking() }});
+        
     while (!wasKeyboardHit()) {
-        niteRc = unsafe { userTracker.readFrame(userTrackerFrameRef) };
-
-        if niteRc != NiteStatus_NITE_STATUS_OK {
-            println!("Get next frame failed");
-            continue;
-        }
-
-        let users = unsafe {
-            std::slice::from_raw_parts(
-                userTrackerFrameRef.as_ref().unwrap().m_users.m_data,
-                userTrackerFrameRef.as_ref().unwrap().m_users.m_size as usize,
-            )
-        };
-        for i in 0..users.len() {
-            let user = users[i];
-
-            updateUserState(user._base, unsafe {
-                (*(userTrackerFrameRef))
-                    .m_pFrame
-                    .as_mut()
-                    .unwrap()
-                    .timestamp
-            });
-            if user._base.state & 2 != 0 {
-                println!("Attempting to start Skeleton Tracking...");
-                userTracker.startSkeletonTracking(user._base.id);
-            } else if user._base.skeleton.state == NiTE::NiteSkeletonState_NITE_SKELETON_TRACKED {
-                let head: NiTE::NiteSkeletonJoint = user._base.skeleton.joints[0];
-                if head.positionConfidence > 0.5 {
-                    println!(
-                        "{}. ({}, {}, {})",
-                        user._base.id, head.position.x, head.position.y, head.position.z
-                    );
-                } else {
-                    println!("{}", head.positionConfidence);
-                }
+        unsafe {
+            if (nite4vr::head.getPosition().x != 0 as f32 || nite4vr::head.getPosition().y != 0 as f32 || nite4vr::head.getPosition().z != 0 as f32)
+            {                
+                println!("({}, {}, {})", nite4vr::head.getPosition().x, nite4vr::head.getPosition().y, nite4vr::head.getPosition().z);                
             }
+            
+            thread::sleep(time::Duration::from_millis(34));
         }
     }
-
-    println!("Keyboard Interupt");
-    unsafe { NiTE::niteShutdown() };
+    
+    unsafe {nite4vr::niteShutdown()};
 }
 
 fn wasKeyboardHit() -> bool {
@@ -134,16 +74,4 @@ fn wasKeyboardHit() -> bool {
     }
 
     return false;
-}
-
-fn updateUserState(user: NiTE::NiteUserData, ts: u64) {
-    if user.state & 2 != 0 {
-        println!("User #{}: New", user.id);
-    } else if user.state & 1 != 0 {
-        println!("User #{}: Visible", user.id);
-    } else if user.state & 1 == 0 {
-        println!("User #{}: Out of scene", user.id);
-    } else if user.state & 4 != 0 {
-        println!("User #{}: Lost", user.id);
-    }
 }
